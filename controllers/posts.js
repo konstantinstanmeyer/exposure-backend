22dc919c1c1c6b300ad3af62d438bc1f31d9f6d2
KonstantinStanmeyer <konstantin@stanmeyer.com>
2023-02-05 09:43:15 -0700

    working posts upload

--- controllers/posts.js
+++ controllers/posts.js
@@ -6,9 +6,9 @@ export const postAddProduct = async (req,res) => {
     if (!user.userId) return res.status(400).json({message: "Not Authorized"});
 
     try {
-        const { category, description, genre, imageUrl } = req.body;
+        const { category, description, subCategory, imageUrl, title } = req.body;
 
-        const post = new Post({ category: category, description: description, genre: genre, creator: user.userId, imageUrl: imageUrl })
+        const post = new Post({ title: title, category: category, description: description, subCategory: subCategory, creator: user.userId, imageUrl: imageUrl })
 
         const response = await post.save();
 
